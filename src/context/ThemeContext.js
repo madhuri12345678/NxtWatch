d06ab82e717c7a9db2cc35535db474c2ef0c4a7f
madhuri12345678 <madhuri200226@gmail.com>
2024-11-09 10:51:57 +0000

    NxtWatch

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const ThemeContext = React.createContext({
+  isDarkTheme: false,
+  toggleTheme: () => {},
+  savedVideosList: [],
+  savedVideoButtonClicked: () => {},
+})
+export default ThemeContext
